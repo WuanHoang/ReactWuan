0f59a913cf799f1940b79022282c32f5037cc69b
trashinbin <minhquanhoang2606@gmail.com>
2023-04-11 00:01:26 +0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
+import {Routes, Route} from 'react-router-dom'
 import Test from "./ListGroup";
+import NavBar from "./NavBar";
 
 function App()
 {
+  const props= ['orange', 'red' , 'green'];
+  const props2 = ['blue', 'orange', 'green'];
+  const props3 = ['red', 'green', 'white'];
   return (
     <>
       <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="./Home">Home</a>
-              <a className="nav-link" href="./Features">Features</a>
-              <a className="nav-link" href="./Pricing">Pricing</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-        <Test></Test>
+        <NavBar></NavBar>
+        <h1> LIST GROUP </h1>
+        <Routes>
+          <Route path="/Features" element = {<Test item = {props} heading='test'></Test>}/>
+          <Route path="/Pricing" element = {<Test item = {props2} heading='test'></Test>}/>
+          <Route path="/Home" element = {<Test item = {props3} heading='test'></Test>}/>
+        </Routes>
       </div>
     </>
   )
--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -7,7 +7,7 @@ interface Prop {
 
 
 
-const items = ["Mars", 'Earth', 'Venus', 'Apollo'];
+// const items = ["Mars", 'Earth', 'Venus', 'Apollo'];
 
 
 function listPrint(item: string, count: number) {
@@ -16,13 +16,12 @@ function listPrint(item: string, count: number) {
 }
 
 
-function Test() {
+function Test(prop: Prop) {
     const [countNumber, setCountNumber] = useState(-1);
     return (
         <>
-        <h1> LIST GROUP </h1>
             <ul className="list-group">
-                {items.map((item, count) => (
+                {prop.item.map((item, count) => (
                     <li className={countNumber === count ? 'list-group-item active' : 'list-group-item'} key={item} onClick={()=> setCountNumber(count)}>{item}</li>
                 ))}
             </ul>
